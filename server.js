e15f7cc4644afc0039e73c0491f300a241c3c717
fernandogelin <fernando@fernandogelin.com>
2020-03-30 13:00:15 -0400

    feat: add dev script

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
 
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 const endpoint = 'https://api.github.com/graphql' 
 
